17ef6d938834a321e3e364e9d9ca06edca18d688
Yunus Yen <yunyeng@google.com>
2020-08-04 23:41:27 +0000

    Make ExponentialIndexFunction and UniqueValueFunction public
    -Add LICENSE to the classes missing
    
    Change-Id: If0a1170ca06562bd693f07d6e49a4986ec1acb0d

--- src/main/java/org/wfanet/anysketch/UniqueValueFunction.java
+++ src/main/java/org/wfanet/anysketch/UniqueValueFunction.java
@@ -1,3 +1,17 @@
+// Copyright 2020 The Any Sketch Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package org.wfanet.anysketch;
 
 /**
@@ -5,7 +19,7 @@ package org.wfanet.anysketch;
  * For Unique Value Function, if all aggregated values are the same then result of aggregation is
  * equal to the value. Otherwise it is -1. Note that in the proto the destructor will be 0.
  */
-class UniqueValueFunction implements ValueFunction {
+public class UniqueValueFunction implements ValueFunction {
 
   /**
    * The name of this value function is UniqueValueFunction.
